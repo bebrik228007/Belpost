fd2fba044d9a5d4b3e4b0209101b6ca0f51bdec9
Руслан Васинкевич <vasinkevichruslan@gmail.com>
2026-01-22 22:21:22 +0300

    окно v2 регистрация

--- MainWIndow.xaml.cs
+++ MainWIndow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Linq;
 
 namespace Belpost.Auth
 {
@@ -12,46 +13,54 @@ namespace Belpost.Auth
         private void Login_Click(object sender, RoutedEventArgs e)
         {
             string login = LoginBox.Text;
-            string password = PasswordBox.Password;
+            string password;
 
-            MessageBox.Show("логин " + login, "пароль " + password);
+            if (PasswordBox.Visibility == Visibility.Visible)
+                password = PasswordBox.Password;
+            else
+                password = VisibleTextBox.Text;
 
-            using (var db = new AppDb()) //!!! нет хеширования
+
+            using (var db = new AppDb()) 
             {
                 var user = db.Users.FirstOrDefault(u => u.Username == login);
 
                 if (user == null)
                 {
                     MessageBox.Show("Такого пользователя не существует!");
+                    return;
                 }
 
-                if (user?.PasswordHash == password && user != null)
+                if (user.PasswordHash == password)
                 {
                     MessageBox.Show($"Добро пожаловать, {user.Role}!");
                 }
                 else
                 {
-                    MessageBox.Show("Неверный пароль! ");
+                    user.FailedAttempts += 1;
+                    db.SaveChanges(); 
+                    MessageBox.Show("Неверный пароль!");
                 }
             }
         }
 
         private void CheckBox1_Checked(object sender, RoutedEventArgs e)
         {
-            PasswordBox.Visibility = Visibility.Collapsed;
             VisibleTextBox.Text = PasswordBox.Password;
+            PasswordBox.Visibility = Visibility.Collapsed;
             VisibleTextBox.Visibility = Visibility.Visible;
         }
 
         private void CheckBox1_UnChecked(object sender, RoutedEventArgs e)
         {
+            PasswordBox.Password = VisibleTextBox.Text;
             PasswordBox.Visibility = Visibility.Visible;
-            VisibleTextBox.Visibility = Visibility.Hidden;
+            VisibleTextBox.Visibility = Visibility.Collapsed;
         }
 
         private void V_Budushem_Dobavlu(object sender, RoutedEventArgs e)
         {
-
+            
         }
     }
 }
